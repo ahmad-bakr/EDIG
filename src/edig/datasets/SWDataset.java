8853e7b41b37bd37b219aa60e8127dcde9fd5762
Ahmad Bakr <ahmad.bakr@espace.com.eg>
2012-02-20 22:27:23 +0200

    parse SW dataset

--- src/edig/datasets/SWDataset.java
+++ src/edig/datasets/SWDataset.java
@@ -1,46 +1,101 @@
 package edig.datasets;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
+import org.jsoup.Jsoup;
+
 import edig.entites.Document;
+import edig.entites.DocumentManager;
 
 public class SWDataset implements DatasetLoader{
 
-	@Override
-	public Hashtable<String, Document> loadDocuments() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	private Hashtable<String, Document> documentHash;
+	private String datasetPath;
+	private int numberOfDocument;
+	private ArrayList<String> originalClasses ;
+	private Hashtable<String, Integer> classDocumentCount;
+	private ArrayList<String> documentsIDS;
 
-	@Override
-	public Document getDocument(String documentID) {
-		// TODO Auto-generated method stub
-		return null;
+	public SWDataset(String path) {
+		this.documentsIDS = new ArrayList<String>();
+		this.originalClasses = new ArrayList<String>();
+		this.datasetPath = path;
+		this.documentHash = new Hashtable<String, Document>();
+		this.numberOfDocument =0;
+		this.classDocumentCount = new Hashtable<String, Integer>();
 	}
-
+	
 	@Override
-	public int numberOfDocuments() {
-		// TODO Auto-generated method stub
-		return 0;
+	public ArrayList<String> getOriginalClasses() {
+		return this.originalClasses;
 	}
-
-	@Override
+	
+	@Override	
 	public ArrayList<String> getDocumentsIDS() {
-		// TODO Auto-generated method stub
-		return null;
+		return documentsIDS;
 	}
 
 	@Override
 	public int getNumberOfDocumentsInClass(String className) {
 		// TODO Auto-generated method stub
-		return 0;
+		return this.classDocumentCount.get(className);
 	}
 
+	
 	@Override
-	public ArrayList<String> getOriginalClasses() {
-		// TODO Auto-generated method stub
-		return null;
+	public Hashtable<String, Document> loadDocuments() throws Exception {
+		File dir = new File(this.datasetPath);
+    String[] categories = dir.list();
+    for (int i = 0; i < categories.length; i++) {
+    	String categoryName = categories[i];
+    	this.originalClasses.add(categoryName);
+  		String[] docsInCategory = new File(this.datasetPath+"/"+categoryName).list();
+  		this.classDocumentCount.put(categoryName, docsInCategory.length);
+    	for (int j = 0; j < docsInCategory.length; j++) {
+    		this.numberOfDocument++;
+    		String documentName = docsInCategory[j];
+    		File input = new File(this.datasetPath+"/"+categoryName+"/"+documentName);
+    		org.jsoup.nodes.Document doc = Jsoup.parse(input, "UTF-8", "");
+    		String title =  categoryName+" "+ doc.title().replaceAll("(\\r|\\n)", ". ");
+				String body = doc.text().replaceAll("(\\r|\\n)", ". ").replaceAll("\\s+", " ");//getFirstNWords(doc.text().;
+				Document stemmedDocument = DocumentManager.createDocument(documentName, title, body);
+				stemmedDocument.setOrginalCluster(categoryName);
+
+				this.documentHash.put(stemmedDocument.getId(),stemmedDocument);
+				this.documentsIDS.add(stemmedDocument.getId());
+			}// end loop over documents
+		}// end loop over categories
+		return this.documentHash;
+	}
+	
+	@Override
+	public Document getDocument(String documentID) {
+		return this.documentHash.get(documentID);
+	}
+	
+	private String getFirstNWords(String s){
+		String [] arr = s.split(" ");
+		String str = "";
+		for (int i = 0; i < 200; i++) {
+			if (i >= arr.length) break;
+			str += arr[i];
+		}
+		
+		return str;
+	}
+	
+	@Override
+	public int numberOfDocuments() {
+		return this.numberOfDocument;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		SWDataset sw = new SWDataset("/media/disk/master/Master/datasets/SW");
+		sw.loadDocuments();
+		
+		
 	}
 
 }
