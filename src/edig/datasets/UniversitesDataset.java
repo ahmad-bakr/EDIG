025cd675eaf5f5e8fc69f56ada12b66cf6fe5e14
Ahmad Bakr <ahmad.bakr@espace.com.eg>
2012-02-21 22:14:31 +0200

    parse four univetsites dataset

--- src/edig/datasets/UniversitesDataset.java
+++ src/edig/datasets/UniversitesDataset.java
@@ -1,12 +1,37 @@
 package edig.datasets;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
+import org.jsoup.Jsoup;
+
 import edig.entites.Document;
+import edig.entites.DocumentManager;
 
 public class UniversitesDataset extends Dataset {
+	
+	private Hashtable<String, Document> documentHash;
+	private String datasetPath;
+	private int numberOfDocument;
+	private ArrayList<String> originalClasses ;
+	private Hashtable<String, Integer> classDocumentCount;
+	private ArrayList<String> documentsIDS;
+	
+ public UniversitesDataset(String path) {
+		this.documentsIDS = new ArrayList<String>();
+		this.originalClasses = new ArrayList<String>();
+		this.datasetPath = path;
+		this.documentHash = new Hashtable<String, Document>();
+		this.numberOfDocument =0;
+		this.classDocumentCount = new Hashtable<String, Integer>();
+ }
+	
+	
 		public void prepareDataset(){
 			String originalPath = "/media/disk/master/Master/datasets/webkb";
 			String modifiedPath = "/media/disk/master/Master/datasets/four_universites";
@@ -23,39 +48,73 @@ public class UniversitesDataset extends Dataset {
 	    }
 		}
 
-		@Override
-		public Hashtable<String, Document> loadDocuments() throws Exception {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
+		
 		@Override
 		public Document getDocument(String documentID) {
-			// TODO Auto-generated method stub
-			return null;
+			return this.documentHash.get(documentID);
 		}
 
 		@Override
 		public int numberOfDocuments() {
-			// TODO Auto-generated method stub
-			return 0;
+			return this.numberOfDocument;
 		}
 
 		@Override
 		public ArrayList<String> getDocumentsIDS() {
-			// TODO Auto-generated method stub
-			return null;
+			return documentsIDS;
 		}
 
 		@Override
 		public int getNumberOfDocumentsInClass(String className) {
-			// TODO Auto-generated method stub
-			return 0;
+		 return this.classDocumentCount.get(className);
 		}
 
 		@Override
 		public ArrayList<String> getOriginalClasses() {
-			// TODO Auto-generated method stub
-			return null;
+			return this.originalClasses;
+		}
+		
+		
+		private String getFirstNWords(String s){
+			String [] arr = s.split(" ");
+			String str = "";
+			for (int i = 0; i < 250; i++) {
+				if (i >= arr.length) break;
+				str += arr[i];
+			}
+			
+			return str;
+		}
+
+
+		@Override
+		public Hashtable<String, Document> loadDocuments() throws Exception {
+			File dir = new File(this.datasetPath);
+	    String[] categories = dir.list();
+	    for (int i = 0; i < categories.length; i++) {
+	    	String categoryName = categories[i];
+	    	this.originalClasses.add(categoryName);
+	  		String[] docsInCategory = new File(this.datasetPath+"/"+categoryName).list();
+	  		this.classDocumentCount.put(categoryName, docsInCategory.length);
+	    	for (int j = 0; j < docsInCategory.length; j++) {
+	    		this.numberOfDocument++;
+	    		String documentName = docsInCategory[j];
+	    		File input = new File(this.datasetPath+"/"+categoryName+"/"+documentName);
+	    		org.jsoup.nodes.Document doc = Jsoup.parse(input, "UTF-8", "");
+	    		String title = categoryName+" "+ doc.head().text().replaceAll("(\\r|\\n)", ". ");
+					String body = getFirstNWords(doc.body().text().replaceAll("(\\r|\\n)", ". "));
+					Document stemmedDocument = DocumentManager.createDocument(documentName, title, body);
+					stemmedDocument.setOrginalCluster(categoryName);
+					this.documentHash.put(stemmedDocument.getId(),stemmedDocument);
+					this.documentsIDS.add(stemmedDocument.getId());
+				}// end loop over documents
+			}// end loop over categories
+			return this.documentHash;
+		}
+
+		
+		public static void main(String[] args) {
+			UniversitesDataset u = new UniversitesDataset("");
+			u.prepareDataset();
 		}
 }
